6c4c8ce8321d7f76e8803de4cf9f25cef3e703ca
Mert Geler <geler818@gmail.com>
2023-12-19 01:46:01 +0300

    proje tamam

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useReducer } from "react";
 
-import TotalDisplay from './components/TotalDisplay';
-import CalcButton from './components/CalcButton';
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
+
+import {
+  ADD_ONE,
+  CHANGE_OPERATION,
+  addOne,
+  applyNumber,
+  clearDisplay,
+  memoryAdd,
+  memoryClear,
+  memoryRecall,
+} from "./actions";
+import reducer, { initialState } from "./reducers";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const handleNumberClick = (e) => {
+    const number = e.target.value;
+    dispatch(applyNumber(number));
+  };
+
+  const handleOperationChange = (e) => {
+    const operation = e.target.value;
+    dispatch({ type: CHANGE_OPERATION, payload: operation });
+  };
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -13,47 +36,58 @@ function App() {
       <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-
-            <TotalDisplay value={0} />
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation">
+                <b>Operation:</b> {state.operation}
+              </span>
+              <span id="memory">
+                <b>Memory:</b> {state.memory}
+              </span>
             </div>
 
             <div className="row">
-              <CalcButton value={"M+"} />
-              <CalcButton value={"MR"} />
-              <CalcButton value={"MC"} />
+              <CalcButton onClick={() => dispatch(memoryAdd())} value={"M+"} />
+              <CalcButton
+                onClick={() => dispatch(memoryRecall())}
+                value={"MR"}
+              />
+              <CalcButton
+                onClick={() => dispatch(memoryClear())}
+                value={"MC"}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+              <CalcButton value={1} onClick={handleNumberClick} />
+              <CalcButton value={2} onClick={handleNumberClick} />
+              <CalcButton value={3} onClick={handleNumberClick} />
             </div>
 
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton value={4} onClick={handleNumberClick} />
+              <CalcButton value={5} onClick={handleNumberClick} />
+              <CalcButton value={6} onClick={handleNumberClick} />
             </div>
 
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton value={7} onClick={handleNumberClick} />
+              <CalcButton value={8} onClick={handleNumberClick} />
+              <CalcButton value={9} onClick={handleNumberClick} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"} />
-              <CalcButton value={"*"} />
-              <CalcButton value={"-"} />
+              <CalcButton value={"+"} onClick={handleOperationChange} />
+              <CalcButton value={"*"} onClick={handleOperationChange} />
+              <CalcButton value={"-"} onClick={handleOperationChange} />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"} />
+              <CalcButton
+                value={"CE"}
+                onClick={() => dispatch(clearDisplay())}
+              />
             </div>
-
           </form>
         </div>
       </div>
